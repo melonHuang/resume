ada23e2b7aec6997dbc97e3c082117c827804d65
melonHuang <228941629@qq.com>
2017-12-22 11:15:11 +0800

    rebuild

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-./markdown-resume/bin/md2resume html --template unstyled ./huangwei.md .
+./markdown-resume/bin/md2resume html --template modern ./huangwei.md .
+./markdown-resume/bin/md2resume pdf --template modern ./huangwei.md .
